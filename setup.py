6081433fbc56c63dd229bfbe62bf5aba7ab5a469
Arseniy Kononov <kononovarseniy@gmail.com>
2020-10-16 23:07:20 +0700

    PyPI package created

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="fti-fompy",
+    version="0.0.0",
+    author="Arseniy Kononov",
+    author_email="a.kononov1@g.nsu.ru",
+    description="Routines for the course \"Physical Fundamentals of Microelectronics\"",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kononovarseniy/fompy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'fdint',
+        'numpy',
+        'scipy'
+    ],
+    python_requires='>=3.6',
+)
