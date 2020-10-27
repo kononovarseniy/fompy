08dc8e3e8732192be6c7ac1d98d789c1402864fa
Artyom Bazhenov <tema.bajen@gmail.com>
2020-10-27 18:19:00 +0700

    Update README.md and 's docstring

--- fompy/__init__.py
+++ fompy/__init__.py
@@ -1,1 +1,13 @@
+"""
+This package contains modules for solving semiconductor physics problems.
+
+The modules include functions to calculate different properties of materials
+and classes implementing models of materials: intrinsic and doped semiconductors, metals,
+p-n junctions and metal-semiconductor junctions, various crystal lattices, etc.
+Also, there are instruments to handle unit conversions, since the unit system
+used in all computations is cgs (the Gaussian unit system).
+
+More information can be found in the descriptions of the modules.
+"""
+
 VERSION = '0.1.0'
